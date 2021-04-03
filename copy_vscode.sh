32ec467fc3ad467a81d672d65a4fe3d6c5178cb3
Stan <iamjacke@gmail.com>
2021-04-03 17:20:59 +0300

    Add VSCode

---
+++ copy_vscode.sh
@@ -0,0 +1,3 @@
+cp ~/Library/Application\ Support/Code/User/settings.json ./vscode/
+cp ~/Library/Application\ Support/Code/User/keybindings.json ./vscode/
+cp -r ~/Library/Application\ Support/Code/User/snippets ./vscode/
